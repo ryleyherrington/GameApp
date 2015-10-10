d8864afb510c2070f0371fb90a212b8708b1cc5c
Ryley Herrington <ryleyherrington@gmail.com>
2015-10-10 09:29:19 -0700

    Testing writing out json

--- gamersforecast-1094/main.py
+++ gamersforecast-1094/main.py
@@ -18,7 +18,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+		json = open("test.json", "r")
+		self.response.write(json)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
